873058699574c3de9892115cdaae4634a59ed004
ManuOmulo <omuloemmanuel@gmail.com>
2024-01-07 11:28:42 +0300

    Added SortedArray class and methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) {
+    int[] arr = SortedArray.getIntegers(5);
+    SortedArray.printArray(arr);
+    System.out.println(Arrays.toString(SortedArray.sortIntegers(arr)));
 
   }
 }
---
+++ src/SortedArray.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortedArray {
+
+  public static int[] getIntegers(int size) {
+    int counter = 0;
+    int[] intArray = new int[size];
+
+    while (counter < size) {
+      Scanner sc = new Scanner(System.in);
+      System.out.printf("Enter %d numbers: %n", size);
+      String input = sc.nextLine();
+      int number = Integer.parseInt(input);
+      intArray[counter] = number;
+      counter += 1;
+    }
+
+    return intArray;
+  }
+
+  public static void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.printf("Element %d contents %d %n", i, array[i]);
+    }
+  }
+
+  public static int[] sortIntegers(int[] array) {
+    int[] sortedArray = Arrays.copyOf(array, array.length);
+
+    Arrays.sort(sortedArray);
+
+    for (int i = 0; i < sortedArray.length; i++) {
+      for (int j = 1; j < sortedArray.length; j++) {
+        if (j > i) {
+          int temp = sortedArray[i];
+          sortedArray[i] = sortedArray[j];
+          sortedArray[j] = temp;
+        }
+      }
+    }
+
+    return sortedArray;
+  }
+}
